c0a83058f6b5824096483bc2e3433bdf43d74ce0
Priscila Galassini <priscilagalassini@gmail.com>
2024-11-26 15:37:59 -0300

    Feat: Classes Collections e Widgets

---
+++ app/src/main/java/br/com/tecnology/galassini/rotadaf/activitys/CollectionsActivity.kt
@@ -0,0 +1,13 @@
+/*
+ * Copyright © 2024 Priscila De Oliveira Galassini
+ * Todos os direitos reservados.
+ *
+ * Este código é protegido por direitos autorais e não pode ser copiado, modificado ou redistribuído sem permissão expressa.
+ * Se você tiver permissão para usar este código, ele é fornecido "no estado em que se encontra", sem garantia de qualquer tipo.
+ */
+
+
+package br.com.tecnology.galassini.rotadaf.activitys
+
+class CollectionsActivity {
+}
